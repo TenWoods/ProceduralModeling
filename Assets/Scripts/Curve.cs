62b59fb8751fb95bc0b9421e5e259687bb59f78d
十木 <yangwx0905@sina.com>
2018-10-22 23:41:25 +0800

    能生成n阶的贝塞尔曲线

---
+++ Assets/Scripts/Curve.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralModeling
+{
+	[System.Serializable]
+	//生成N阶贝塞尔曲线
+	public class Curve
+	{
+		[SerializeField]
+		//曲线的控制点
+		private	Vector3[] points;
+
+		//获取贝塞尔曲线上t值所对应的点[0, 1]
+		public Vector3 GetPoint(float t) 
+		{
+			//贝塞尔曲线阶数
+			int n = points.Length - 1;
+			//将t值控制在[0,1]
+			if (t > 1f)
+				t = 1f;
+			if (t < 0f)
+				t = 0f;
+			int count = points.Length;
+			Vector3[] tmp, lastPoints;
+			lastPoints = points;
+			for (int i = 0; i < n; i++)
+			{
+				tmp = new Vector3[count - 1];
+				for (int j = 0; j < lastPoints.Length - 1; j++)
+				{
+					tmp[j] = (lastPoints[j + 1] - lastPoints[j]) * t + lastPoints[j];
+				}
+				lastPoints = tmp;
+				count--;
+			}
+			return lastPoints[0];
+		}
+	}
+}
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProceduralModeling;
+
+public class Pipe : ProceduralModelingBase 
+{
+	[SerializeField]
+	//曲线类
+	private Curve curve;
+	[SerializeField]
+	//曲线上点的个数
+	private int curveSegment;
+	
+	protected override void Start()
+	{
+		base.Start();
+		curve = new Curve();
+	}
+
+	protected override Mesh Build()
+	{
+		Mesh mesh = new Mesh();
+		List<Vector3> vertices = new List<Vector3>();
+		List<Vector2> uvs = new List<Vector2>();
+		List<Vector3> normals = new List<Vector3>();
+		List<int> triangles = new List<int>();
+		
+		return mesh;
+	}
+}
